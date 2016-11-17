e71d522c01a1f93b110385b4e0f1ecc291d20884
Roberto Oliveira <rdutra@linux.vnet.ibm.com>
2016-11-17 12:24:57 -0200

    tools-sca: fix bugs in run and check the execution status.
    - Change the tool to use the script from fdpr_wrap to run
    the fdpr.
    - Check the fdpr exit status and print errors, if their occur,
    informing the phase the execution failed.
    
    Change-Id: I55fe28db16caddb951f98f276a5d0db0cdc75654
    Signed-off-by: Roberto Oliveira <rdutra@linux.vnet.ibm.com>

--- src/controller.py
+++ src/controller.py
@@ -17,18 +17,57 @@ limitations under the License.
 
 	Contributors:
 		* Rafael Sene <rpsene@br.ibm.com>
+		* Roberto Oliveira <rdutra@br.ibm.com>
 """
 
 import core
 import sys
+import os
 
-FDPR='/opt/ibm/fdprpro/bin/fdprpro'
+FDPRPRO='/opt/ibm/fdprpro/bin/fdprpro'
+FDPR_WRAP='/opt/ibm/fdprpro/bin/fdpr_instr_prof_jour'
 
-def runsca(binary_path, binary_name, opt_value, warn_value, verbose_value, processor_value):
-	if [core.cmdexists(FDPR)]:
-		core.execute(FDPR + " -a instr -p " + binary_path + " -w " + str(warn_value) + " -v " + str(verbose_value) + " -f ./" + binary_name + ".prof -fd 999 -o " + binary_name + ".instr")
-		core.execute(binary_path)
-		core.execute(FDPR + " -a opt -O" + str(opt_value) + " -w " + str(warn_value) + " -v " + str(verbose_value) + " -m " + processor_value + " -p " + binary_path + " -f ./" + binary_name +".prof -o ./" + binary_name + ".fdpr.opt -j " + binary_name + ".xml")
+def runsca(binary_cmd, binary_name, opt_value, warn_value, verbose_value, processor_value):
+	"""
+	Run the SCA tool
+	"""
+	if not core.cmdexists(FDPRPRO):
+		sys.stderr.write("fdprpro package is not installed. Install it and and try again.\n")
+		sys.exit(0)
+	elif not core.cmdexists(FDPR_WRAP):
+		sys.stderr.write("fdpr_wrap package is not installed. Install it and try again.\n")
+		sys.exit(0)
 	else:
-		sys.stderr.write("FDPR is not installed. Fix it and try again.")
+		# Available optimization levels in FDPR: O, O2, O3, O4
+		if opt_value == '1':
+			opt_value = ''
+
+		# Create flags to be passed to fdpr
+		opt_flag = "-O" + str(opt_value)
+		warn_flag = "-w " + str(warn_value)
+		verbose_flag = "-v " + str(verbose_value)
+		processor_flag = "-m " + processor_value
+
+		# Export flags in system environment
+		os.environ['FDPR_OPT_FLAGS'] = opt_flag + " " + warn_flag + " " + verbose_flag + " " + processor_flag
+
+		status = core.execute(FDPR_WRAP + " " + binary_cmd)
+		check_exit_status(status)
+
+		# TODO: Integrate the output part here
+		jour_file = binary_name + "-jour.xml"
+
+
+def check_exit_status(status):
+	"""
+	Check execution exit status
+	"""
+	if status == 1:
+		sys.stderr.write('FDPR failed during application instrumentation.\n')
+		sys.exit(0)
+	elif status == 2:
+		sys.stderr.write('FDPR failed during application profiling.\n')
+		sys.exit(0)
+	elif status == 3:
+		sys.stderr.write('FDPR failed during journal production.\n')
 		sys.exit(0)
